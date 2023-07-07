0c7b2660704b227768603683da8355728203538a
tai-metacrew <tai.vo@metacrew.vn>
2023-07-07 14:29:37 +0700

    deploy

---
+++ assets/SettingPage-6e15c3f8.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-1b13197e.js";import{C as e}from"./Container-cbf9fa82.js";const i=()=>t.jsx(e,{type:"app",children:t.jsx("p",{children:"SettingPage"})});export{i as SettingPage};
